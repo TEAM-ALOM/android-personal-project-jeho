8cc663ab7249b5b3eba752ee248355b43145006f
Jeho <jjh00704.gmail.com>
2023-05-21 20:34:56 +0900

    interface change

--- android-personal-project-jeho/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ android-personal-project-jeho/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -6,6 +6,9 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.`main_screen.xml`)
+        setContentView(R.layout.main_screen)
+        setContentView(R.layout.signup_screen)
+        setContentView(R.layout.timetable_screen)
     }
 }
+
